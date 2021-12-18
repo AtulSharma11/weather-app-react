d00d57ed3a0795cd06a7547665f4b0781d7746d8
Atul <atulsharma.sa1@gmail.com>
2021-12-18 16:57:31 -0500

    Styled weather Box

--- src/App.js
+++ src/App.js
@@ -28,6 +28,12 @@ function App() {
           <div className="location">Toronto CA</div>
           <div className="date">{dateBuilder(new Date())}</div>
         </div>
+        <div className="weather-box">
+          <div className="temp">
+            15°c
+          </div>
+          <div className="weather">Sunny</div>
+        </div>
       </main>
     </div>
   );
