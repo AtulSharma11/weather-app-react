4ac6118750f1c0440fafa8f18380e1bb81c6bb8a
Atul <atulsharma.sa1@gmail.com>
2021-12-18 16:35:00 -0500

    Added Date and Location Box

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ const api={
   base: 'https://api.openweathermap.org/data/2.5/'
 }
 function App() {
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+    
+    return `${day} ${date} ${month} ${year}`
+  }
   return (
     <div className="app">
       <main>
@@ -12,6 +23,10 @@ function App() {
           className = "search-bar"
           placeholder = "Search..."
           />
+        </div>
+        <div className="location-box">
+          <div className="location">Toronto CA</div>
+          <div className="date">{dateBuilder(new Date())}</div>
         </div>
       </main>
     </div>
