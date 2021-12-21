6ea9f9939c8a34f89bce45cee635f3099b7854f9
Atul <atulsharma.sa1@gmail.com>
2021-12-20 21:06:34 -0500

    Implemented api calls

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import {useState} from 'react';
+
+
 const api={
   key: '9b14e2c5e8080a7ad16dc865ca7314d4',
   base: 'https://api.openweathermap.org/data/2.5/'
 }
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === 'Enter'){
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(res => res.json())
+      .then(result => { 
+        setWeather(result);
+        setQuery('');
+        console.log(result);
+      })
+    } 
+  }
+  
+
+  
+
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -22,21 +43,29 @@ function App() {
           type = "text"
           className = "search-bar"
           placeholder = "Search..."
+          onChange ={e =>setQuery(e.target.value)}
+          value = {query}
+          onKeyPress= {search}
           />
         </div>
+        {(typeof weather.main != 'undefined') ?( 
+      <div>
         <div className="location-box">
-          <div className="location">Toronto CA</div>
+          <div className="location">{weather.name},{weather.sys.country}</div>
           <div className="date">{dateBuilder(new Date())}</div>
         </div>
-        <div className="weather-box">
+        <div className="weather-box"> 
           <div className="temp">
-            15°c
+            {Math.round(weather.main.temp)}°c
           </div>
-          <div className="weather">Sunny</div>
+          <div className="weather">{weather.weather[0].main}</div>
+          {/* {console.log(weather.weather[0].main)} */}
         </div>
+      </div>
+        ):('')}
+    
       </main>
     </div>
   );
 }
-
 export default App;
